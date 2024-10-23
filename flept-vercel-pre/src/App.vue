9ae1897afe7afb6dd5bc19a090befc71773e7725
avoe <inuser027@gmail.com>
2024-10-24 02:54:00 +0400

    init

---
+++ flept-vercel-pre/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <GifDisplay />
+  </div>
+</template>
+
+<script>
+import GifDisplay from './components/GifDisplay.vue';
+
+export default {
+  components: {
+    GifDisplay,
+  },
+};
+</script>
+
+<style>
+/* Global styles for the entire app */
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #1e1e1e; /* Change this color to what you want */
+  font-family: 'Arial, sans-serif';
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Full viewport height */
+}
+</style>
---
+++ flept-vercel-pre/src/components/GifDisplay.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="gif-container">
+    <img :src="gifUrl" alt="Looping GIF" class="gif" />
+    <p class="gif-text">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+import myGif from '../assets/icons8-toolsss.gif';
+
+export default {
+  data() {
+    return {
+      gifUrl: myGif,
+      text: 'Site is under development!',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.gif-container {
+  text-align: center;
+  margin: 20px;
+  background-color: #d3d3d3; /* Lighter background color */
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3); /* Softer shadow */
+}
+
+.gif {
+  width: 100px; /* Adjust the width */
+  height: 100px; /* Adjust the height */
+}
+
+.gif-text {
+  font-size: 1.5em;
+  margin-top: 10px;
+  color: #333; /* Slightly darker text color for contrast */
+}
+</style>
